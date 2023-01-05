70f25329de722dff66c8b36d58776016b3d33fe3
Zlatko Jankovski <100656935+StreetMindZh@users.noreply.github.com>
2023-01-05 10:04:52 +0100

    chris files loaded

--- index.php
+++ index.php
@@ -1,30 +1,116 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World</title>
-    
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
 </head>
-<style>
-    .question{
-        font-size: 30px;
-        color: green;
-    }
 
-    </style>
 <body>
-    <div>
+
+<!-- HEADER -->
+<?php 
+    include "./includes/tools.php";
+    include "./includes/header.php"; 
+?>
+<!-- END:HEADER -->
+
+<div class="container my-4 text-center">
+
+<p class="text-center mt-5">
     <?php
-        echo "<p>Hello World!</p>";
-        $firstName = "Zlatko";
-        $lastName = "Jankovski";
-        echo "Hello $firstName $lastName! <p class=question>how are you?</p>";
-
-        
-
+        $title = "Mein Titel";
+        echo "<strong>" . $title . "</strong>";
     ?>
-    </div>
+</p>
+
+<?php
+    /*
+        echo mit "" verwenden, damit Variablennamen ersetzt werden.
+        $title existiert bereits. Sie wird im vorangehenden php-Tag
+        deklariert und gesetzt.
+    */
+    echo "<p class='text-center mt-5'>
+       <strong>$title</strong>
+    </p>";
+?>
+
+<!-- FORMULAR -->
+<form action="result.php" method="post">
+    <label for="name">Name:</label>
+    <input type="text" id="name" name="name"><br>
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email"><br>
+
+    <p>Geht es dir heute gut?</p>
+    <input type="radio" id="ja" name="radio-mood" value="ja">
+    <label for="ja">Ja</label><br>
+    <input type="radio" id="nein" name="radio-mood" value="nein">
+    <label for="nein">Nein</label><br><br>
+
+    <p>Welche Farben hat die Schweizer Fahne?</p>
+    <input type="checkbox" id="color1" name="color-red" value="rot">
+    <label for="color1">Rot</label><br>
+    <input type="checkbox" id="color2" name="color-yellow" value="gelb">
+    <label for="color2">Gelb</label><br>
+    <input type="checkbox" id="color3" name="color-blue" value="blau">
+    <label for="color3">Blau</label><br>
+    <input type="checkbox" id="color4" name="color-white" value="weiss">
+    <label for="color4">Weiss</label><br><br>
+
+    <p>Welches ist dein lieblingstier?</p>
+    <select class="form-select" id="Animal" name="Animal">
+      <option selected>Auswählen...</option>
+    <?php
+      // Generate the animal names using a while loop.
+      $mammals = array("Rind", "Pferd", "Ziege", "Mensch");
+      $i = 0;
+      $length = count($mammals);
+
+      while ($i < $length) {
+        $m = $mammals[$i];
+
+        /*
+        Ich könnte foolgendes verwenden
+        echo '<option value=" ' . $m '">' . $m . '</option>';
+
+        Es ost aber 
+        
+        
+        */
+        echo '<option value="' . $m . '">' . $m . '</option>';
+        $i = $i + 1; // Short form: $i1+;
+      }
+    ?>
+
+
+    </select>
+
+    <input type="submit" value="Abschicken">
+</form>
+<!-- END:FORMULAR -->
+
+</div>
+
+<!-- FOOTER -->
+<?php include "./includes/footer.php"; ?>
+<!-- END:FOOTER -->
+    
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+    integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+  </script>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+    integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+  </script>
 </body>
+
 </html>
